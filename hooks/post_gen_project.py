d36f19f71f81d35b63cb01ed4499f7fe7ecf2945
Sandeep Murthy <smurthy@protonmail.ch>
2017-03-22 16:33:45 +0000

    Tweak to hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,13 +13,13 @@ BASE_DIR = os.getcwd()
 SUBMODULE_TARGET_DIR = os.path.join(BASE_DIR, 'src')
 
 def create_git_repo():
-	print 'Creating Git repo in {}'.format(BASE_DIR)
+	#print 'Creating Git repo in {}'.format(BASE_DIR)
 	check_call(['git', 'init'])
 	check_call(['git', 'add', '.'])
 	check_call(['git', 'commit', '-m', 'Post-project creation initialisation'])
 
 def add_git_submodules():
-	print 'Creating submodules in {}'.format(SUBMODULE_TARGET_DIR)
+	#print 'Creating submodules in {}'.format(SUBMODULE_TARGET_DIR)
 	for name, url in GIT_SUBMODULES.iteritems():
 	    check_call(['git', 'submodule', 'add', '-f', '{}'.format(url), '{}'.format(os.path.join(SUBMODULE_TARGET_DIR, name))])
 	    check_call(['cd', os.path.join(SUBMODULE_TARGET_DIR, name)])
