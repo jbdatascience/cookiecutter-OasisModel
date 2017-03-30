496ce5093630026b8a9da4bd7ecb89a24d2a9cd7
Sandeep Murthy <smurthy@protonmail.ch>
2017-03-30 22:02:39 +0100

    Tweak to post-project creation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,9 +6,9 @@ import time
 from subprocess import check_call
 
 OASIS_GIT_SUBMODULES = {
-    'oasis_utils': 'https://github.com/OasisLMF/oasis_utils',
-    'oasis_keys_lookup': 'https://github.com/OasisLMF/oasis_keys_lookup',
-    'oasis_keys_server': 'https://github.com/OasisLMF/oasis_keys_server'
+    'oasis_utils': 'git+ssh://git@github.com/OasisLMF/oasis_utils',
+    'oasis_keys_lookup': 'git+ssh://git@github.com/OasisLMF/oasis_keys_lookup',
+    'oasis_keys_server': 'git+ssh://git@github.com/OasisLMF/oasis_keys_server'
 }
 
 
@@ -17,20 +17,22 @@ def create_git_repo():
     check_call(['git', 'add', '.'])
     check_call(['git', 'commit', '-m', 'Post-project creation initialisation'])
 
+
 def add_git_submodules():
     for name, url in OASIS_GIT_SUBMODULES.items():
         check_call(
             [
-                'git', 'submodule', 'add', '-f', '{}'.format(url), os.path.join('src', name)
+                'git', 'submodule', 'add', '-f', url, os.path.join('src', name),
+                'git', 'checkout', 'master'
             ]
         )
-    check_call(['git', 'submodule', 'foreach', '--recursive', 'git', 'remote', 'set-url', 'origin', 'git+ssh://git@github.com/OasisLMF/{}'.format(name)])
-    check_call(['git', 'submodule', 'foreach', '--recursive', 'git', 'checkout', 'master'])
+
 
 if __name__ == '__main__':
-    print('\nCreating Git repo in {}.\n'.format(os.getcwd()))
+    cwd = os.getcwd()
+    print('\nCreating Git repo in {}.\n'.format(cwd))
     time.sleep(2)
     create_git_repo()    
-    print('\nAdding Git submodules in {}.\n'.format(os.path.join(os.getcwd(), 'src')))
+    print('\nAdding Git submodules in {}.\n'.format(os.path.join(cwd, 'src')))
     time.sleep(2)
     add_git_submodules()
