84676ae0c0fc4a902aa9c9d740fc7d120de2ad41
Sandeep Murthy <smurthy@protonmail.ch>
2017-04-20 12:35:47 +0100

    Fix typo in model keys server init

--- {{cookiecutter.project_slug}}/src/keys_server/__init__.py
+++ {{cookiecutter.project_slug}}/src/keys_server/__init__.py
@@ -2,4 +2,4 @@
 Module initialiser.
 '''
 from .utils import *
-from .{{cookiecutter.model_identifer.replace(' ', '').upper()}}KeysLookup import *
+from .{{cookiecutter.model_identifier.replace(' ', '').upper()}}KeysLookup import *
