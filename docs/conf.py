3525b281eefaaf798a5c5c2d0c2b26f23f53d332
Sandeep Murthy <smurthy@protonmail.ch>
2017-07-11 17:25:48 +0100

    Tweak to copright statement in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cookiecutter-OasisModel'
-copyright = u'2017, Sandeep Murthy'
+copyright = u'2017, Oasis Loss Modelling Framework (v. 0.4, 11 July, 2017).'
 author = u'Sandeep Murthy'
 
 # The version info for the project you're documenting, acts as replacement for
